4f24d8eb49679822942ece9111c3a91292e122be
Yan Grenier <ygrenier@ygrenier.com>
2015-06-24 21:20:30 +0200

    Enumérateurs complexe de fichiers. Méthodes yield.

---
+++ Enumerables-Technet/Source/Enumerables-fr/EnumFilesV1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enumerables
+{
+    /// <summary>
+    /// Enumérable parcourant les lignes texte d'un ensemble de fichier
+    /// </summary>
+    public class EnumFilesV1 : IEnumerable<String>
+    {
+
+        private List<String> _Lines;
+
+        /// <summary>
+        /// Création d'un nouvel énumérable
+        /// </summary>
+        public EnumFilesV1(IEnumerable<String> files)
+        {
+            // Initialisation des fichiers
+            this.Files = files.ToArray();
+
+            // On marque la liste des lignes à charger en la mettant à null
+            _Lines = null;
+        }
+
+        void LoadFiles()
+        {
+            // Création de la liste des lignes
+            _Lines = new List<string>();
+
+            if (this.Files != null)
+            {
+                // Pour chaque fichier
+                foreach (var file in Files)
+                {
+                    try
+                    {
+                        // Ouverture d'un lecteur de fichier texte
+                        using (var reader = new StreamReader(file))
+                        {
+                            // Lecture de chaque ligne du fichier
+                            String line;
+                            while ((line = reader.ReadLine()) != null)
+                            {
+                                // On ajoute la ligne dans la liste
+                                _Lines.Add(line);
+                            }
+                        }
+                    }
+                    catch { }   // Si une erreur à la lecture du fichier on passe au prochain fichier
+                }
+            }
+        }
+
+        /// <summary>
+        /// Retourne l'énumérateur des lignes
+        /// </summary>
+        public IEnumerator<string> GetEnumerator()
+        {
+            // Si la liste des lignes est null alors il faut lire les fichiers
+            if (_Lines == null)
+            {
+                // Chargement des fichiers
+                LoadFiles();
+            }
+
+            // Retourne l'énumérateur de la liste
+            return _Lines.GetEnumerator();
+        }
+
+        /// <summary>
+        /// Implémentation de IEnumerator.GetEnumerator() (version non générique)
+        /// </summary>
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        /// <summary>
+        /// Liste des fichiers
+        /// </summary>
+        public String[] Files { get; private set; }
+
+    }
+
+}
--- Enumerables-Technet/Source/Enumerables-fr/Program.cs
+++ Enumerables-Technet/Source/Enumerables-fr/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,6 +18,19 @@ namespace Enumerables
         {
             var items = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
             return asDisposable ? new EnumerableWithEnumeratorDisposable<int>(items, () => Console.WriteLine("Enumérateur Disposé")).AsEnumerable() : items;
+        }
+
+        /// <summary>
+        /// Liste des fichiers sources
+        /// </summary>
+        static IEnumerable<String> GetTestFileNames()
+        {
+            var dir = AppDomain.CurrentDomain.BaseDirectory;
+            return new String[]{
+                Path.Combine(dir, "Files", "Fichier1.txt"),
+                Path.Combine(dir, "Files", "Fichier2.txt"),
+                Path.Combine(dir, "Files", "Fichier3.txt")
+            };
         }
 
         #region Exemple boucles
@@ -150,6 +164,220 @@ namespace Enumerables
 
         #endregion
 
+        #region Exemple énumérateurs complexes
+
+        /// <summary>
+        /// Test de l'énumérateur de fichier première version
+        /// </summary>
+        static void TestFilesV1()
+        {
+            // Création de l'énumérable avec les fichiers de tests
+            var enumerable = new EnumFilesV1(GetTestFileNames());
+
+            // On parcours l'énumérable
+            foreach (var line in enumerable)
+            {
+                Console.WriteLine(line);
+            }
+
+        }
+
+        /// <summary>
+        /// Test de l'énumérateur améliorant un peut la première version
+        /// </summary>
+        static void TestFilesV2()
+        {
+            // Création de l'énumérable avec les fichiers de tests
+            var enumerable = new EnumFilesV2(GetTestFileNames());
+
+            // On parcours l'énumérable
+            foreach (var line in enumerable)
+            {
+                Console.WriteLine(line);
+            }
+
+        }
+
+        /// <summary>
+        /// Test de l'énumérateur de fichiers plus subtile
+        /// </summary>
+        static void TestFilesV3()
+        {
+            // Création de l'énumérable avec les fichiers de tests
+            var enumerable = new EnumFilesV3(GetTestFileNames());
+
+            // On parcours l'énumérable
+            foreach (var line in enumerable)
+            {
+                Console.WriteLine(line);
+            }
+
+            // On parcours l'énumérable et provoque un arrêt prématuré
+            int i = 0;
+            foreach (var line in enumerable)
+            {
+                if (i++ >= 4) break;
+                Console.WriteLine(line);
+            }
+        }
+        
+        #endregion
+
+        #region Exemples méthode yield
+
+        /// <summary>
+        /// Ouvre un nouveau fichier ou retourne null si une erreur à lieu
+        /// </summary>
+        static StreamReader OpenFile(String file)
+        {
+            try
+            {
+                return new StreamReader(file);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+        /// <summary>
+        /// Enumération des fichiers avec une méthode Yield
+        /// </summary>
+        static IEnumerable<String> EnumFilesYield(IEnumerable<String> files)
+        {
+            if (files != null)
+            {
+                // Pour chaque fichier
+                foreach (var file in files)
+                {
+                    // Ouverture d'un lecteur de fichier texte
+                    using (var reader = OpenFile(file))
+                    {
+                        // reader peut être null si une erreur à eu lieu
+                        if (reader != null)
+                        {
+                            // Lecture de chaque ligne du fichier
+                            String line;
+                            do
+                            {
+                                // Lecture d'une ligne, si une erreur à lieu on arrête la boucle
+                                try
+                                {
+                                    line = reader.ReadLine();
+                                }
+                                catch
+                                {
+                                    break;
+                                }
+                                // On envoi la ligne d'énumérable
+                                if (line != null)
+                                    yield return line;
+                            } while (line != null);// Boucle tant qu'on a une ligne
+                        }
+                    }
+                }
+            }
+        }
+        /// <summary>
+        /// Test la méthode yield d'énumération
+        /// </summary>
+        static void TestFilesYield()
+        {
+            // Récupération d'un 'énumérable avec les fichiers de tests
+            var enumerable = EnumFilesYield(GetTestFileNames());
+
+            // On parcours l'énumérable
+            foreach (var line in enumerable)
+            {
+                Console.WriteLine(line);
+            }
+
+            // On parcours l'énumérable et provoque un arrêt prématuré
+            int i = 0;
+            foreach (var line in enumerable)
+            {
+                if (i++ >= 4) break;
+                Console.WriteLine(line);
+            }
+
+        }
+        /// <summary>
+        /// Même méthode que <see cref="EnumFilesYield"/> mais sans gestion des exception
+        /// </summary>
+        static IEnumerable<String> EnumFilesYieldUnprotected(IEnumerable<String> files)
+        {
+            if (files != null)
+            {
+                // Pour chaque fichier
+                foreach (var file in files)
+                {
+                    // Ouverture d'un lecteur de fichier texte
+                    using (var reader = new StreamReader(file))
+                    {
+                        // Lecture de chaque ligne du fichier
+                        String line;
+                        while ((line = reader.ReadLine()) != null)
+                        {
+                            // On ajoute la ligne dans la liste
+                            yield return line;
+                        }
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Méthode yield complexe
+        /// </summary>
+        static IEnumerable<String> YieldComplex()
+        {
+            Random rnd = new Random();
+
+            // Emission directe
+            yield return "Start";
+
+            // Emission dans un switch
+            for (int i = 0; i < 10; i++)
+            {
+                switch (rnd.Next(4))
+                {
+                    case 1:
+                        yield return "Funky 1";
+                        break;
+                    case 2:
+                        continue;
+                    case 3:
+                        yield return "<<<<";
+                        // Emission d'un autre énum
+                        foreach (var line in EnumFilesYield(GetTestFileNames()))
+                        {
+                            // On place une condition
+                            if (line.Contains("x"))
+                                yield return line;
+                        }
+                        yield return ">>>>";
+                        break;
+                    case 0:
+                    default:
+                        yield return "Funky 0";
+                        break;
+                }
+            }
+
+            // Emission directe
+            yield return "End";
+        }
+
+        /// <summary>
+        /// Test la méthode yield complexe
+        /// </summary>
+        static void TestYieldComplex()
+        {
+            foreach (var item in YieldComplex())
+                Console.WriteLine(item);
+        }
+
+        #endregion
+
         static void WaitAndPress()
         {
             Console.WriteLine("Appuyer sur une touche pour continuer...");
@@ -186,6 +414,33 @@ namespace Enumerables
 
             Console.WriteLine("* Test ReverseEnumerator");
             TestReverse();
+            Console.WriteLine();
+            WaitAndPress();
+            #endregion
+
+            #region Test énumérateurs complexes et méthode yield
+            Console.WriteLine("* Enumérateur de fichier V1 (approche basique)");
+            TestFilesV1();
+            Console.WriteLine();
+            WaitAndPress();
+
+            Console.WriteLine("* Enumérateur de fichier V2 (version précédente améliorée)");
+            TestFilesV2();
+            Console.WriteLine();
+            WaitAndPress();
+
+            Console.WriteLine("* Enumérateur de fichier V3 (version avec une approche flux)");
+            TestFilesV3();
+            Console.WriteLine();
+            WaitAndPress();
+
+            Console.WriteLine("* Enumérateur de fichier avec une méthode Yield");
+            TestFilesYield();
+            Console.WriteLine();
+            WaitAndPress();
+
+            Console.WriteLine("* Méthode Yield complexe");
+            TestYieldComplex();
             Console.WriteLine();
             WaitAndPress();
             #endregion
