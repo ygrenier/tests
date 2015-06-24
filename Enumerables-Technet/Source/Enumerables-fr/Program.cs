6bef87e04475cd6eaecf7387ff874a41afd90233
Yan Grenier <ygrenier@ygrenier.com>
2015-06-24 06:49:33 +0200

    Préparation du programme d'exemple en français.

---
+++ Enumerables-Technet/Source/Enumerables-fr/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enumerables
+{
+    class Program
+    {
+
+        /// <summary>
+        /// Retourne une liste d'éléments
+        /// </summary>
+        static int[] GetItems()
+        {
+            return new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        }
+
+        /// <summary>
+        /// Exemple foreach
+        /// </summary>
+        static void ForEach()
+        {
+            // Récupération de l'énumérable
+            IEnumerable<Int32> enumerable = GetItems();
+
+            // Parcours chaque élément dans 'enumerable'
+            foreach (int elm in enumerable)
+            {
+                // ..
+                Console.WriteLine(elm);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("* Exemple ForEach");
+            ForEach();
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+    }
+}
